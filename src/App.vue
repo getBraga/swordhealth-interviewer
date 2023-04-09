d58b269d1053610c515177cd1726c6e5ba9468bb
Ysmael Braga <ysmael.braga@ibm.com>
2023-04-09 12:31:44 -0300

    app vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,71 @@
+
+
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <TheHeaderVue  v-show="allowHeader"/>
+    <router-view class="container-body"/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import service from "./service/service";
+  import TheHeaderVue from "./components/TheHeader.vue";
+
+  
+export default {
+  name: "IndexApp",
+  components: {
+    TheHeaderVue,
+  },
+data(){
+  return {
+    isLoading: false
+  }
+},
+computed: {
+  allowHeader() {
+   
+    return this.$route.name !=='signin' && 
+           this.$route.name !=='signup'
+  }
+},
+
+async created() {
+  try{
+   
+      if(window.localStorage.getItem('token')){
+        const {data}= await service.validToken()
+        const [user]  = data.users
+        this.$store.commit("GET_USER", user)
+      }
+     
+    }catch(err) {
+      console.log('errrorr')
+      window.localStorage.removeItem("token")
+     
+    }
+},
+async updated() {
+  try{
+    this.isLoading = true;
+   await service.validToken()
+
+
+}catch(err) {
+window.localStorage.removeItem("token")
+this.isLoading = false;
+
+     
+}
+}
+}
+</script>
+<style lang="scss">
+
+.container-body {
+  max-width: 1180px;
+  margin: 0 auto;
+  margin-top: 10px;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
