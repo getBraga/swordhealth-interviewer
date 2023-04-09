39a35cd7cd1caecfe4702b334c74c7e3051f4294
Ysmael Braga <ysmael.braga@ibm.com>
2023-04-09 03:39:25 -0300

    import buefy and main.scss

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import buefy from "buefy";
+import "@/assets/scss/main.scss";
+Vue.use(buefy)
 Vue.config.productionTip = false
 
 new Vue({
