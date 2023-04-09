02688cc7a02dd20bfcb93953e019f2cf102e4ae4
Ysmael Braga <ysmael.braga@ibm.com>
2023-04-09 12:21:40 -0300

    component pageLoading

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import buefy from "buefy";
+import PageLoading from "./components/PageLoading.vue"
 import "@/assets/scss/main.scss";
+
 Vue.use(buefy)
 Vue.config.productionTip = false
-
+Vue.component("PageLoading", PageLoading)
 new Vue({
   router,
   store,
