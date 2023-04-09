aa4f6432f93e7dcc2443bfd8188b1b4f4436b525
Ysmael Braga <ysmael.braga@ibm.com>
2023-04-09 12:37:18 -0300

    discovery page

---
+++ src/views/discovery/Discovery.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="discovery">
+     Discovery
+
+    </div>
+  </template>
+  
+  <script>
+  // @ is an alias to /src
+
+  
+  export default {
+    name: 'DiscoveryPage',
+    components: {
+
+    }
+  }
+  </script>
+  
