b543dabf5e1c1d600b4b47c656cb63f2245c98f8
Ysmael Braga <ysmael.braga@ibm.com>
2023-04-09 23:08:29 -0300

    new methods for the project store

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import service from "@/service/service"
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -11,14 +11,58 @@ export default new Vuex.Store({
     }
   },
   getters: {
+    firstName: state => {
+      var name =  state.user.name.replace(/ .*/, '');
+      return name;
+    }
   },
   mutations: {
     GET_USER({user}, payload) {
       user.email = payload.email;
       user.name = payload.displayName
+    },
+    UPDATE_USER({user}, payload) {
+      user.email = payload.email;
+      user.name = payload.displayName
     }
   },
   actions: {
+    async validateToken({commit,}){
+      const {data} = await service.validToken();
+      const [user] = data.users;
+      commit("GET_USER", user)
+    },
+   async updateUser({commit}, payload){
+       const {data} =  await service.updateUser({
+              displayName:payload.displayName,
+              email: payload.email,
+              idToken: window.localStorage.getItem("token"),
+              returnSecureToken: true
+            }) 
+            if(data.idToken){
+              window.localStorage.setItem("token", data.idToken)
+            }
+            commit("UPDATE_USER", data) 
+    },
+    async sigIn({commit}, payload) {
+      const {data} =  await service.signIn({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken:	true
+      })
+      commit("UPDATE_USER", data) 
+      window.localStorage.setItem("token",data.idToken)
+    },
+    async signUp({dispatch}, payload) {
+      const {data} =  await service.signUp({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken:	true
+      })
+      window.localStorage.setItem("token",data.idToken)
+      await dispatch("updateUser",payload )  
+      
+    }
   },
   modules: {
   }
